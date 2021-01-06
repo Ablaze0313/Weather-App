d52baff8fdc9509b3807c0f22321abea76bdb224
Francis Parrenas <201801094@iacademy.edu.ph>
2021-01-07 07:33:09 +0800

    fixed javascript into oop

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,26 +1,27 @@
-const key = 'F7dW04Bt6RUQ1FlxKRwcaDhHF6GLN8FT';
+class Forecast {
+    constructor (){
+        this.key = 'F7dW04Bt6RUQ1FlxKRwcaDhHF6GLN8FT';
+        this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/';
+        this.cityURI= 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    }
+    async updateCity(city){
+        const cityDets = await this.getCity(city);
+        const weather = await this.getWeather(cityDets.Key);
+        return { cityDets, weather };
+    }
+    async getCity(city){
+        const query = `?apikey=${this.key}&q=${city}`;
+        const response = await fetch(this.cityURI + query);
+        const data = await response.json();
 
-//get weather information
-const getWeather = async(id)=>{
-    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
-    const query = `${id}?apikey=${key}`;
+        return data[0];
+    }
 
-    const response= await fetch( base+ query);
-    const data= await response.json();
+    async getWeather(id){
 
+        const query = `${id}?apikey=${this.key}`;
+        const response= await fetch( this.weatherURI+ query);
+        const data= await response.json();
     return data[0];
-};
-
-
-//get city information
-const getCity = async (city) => {
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
-    const query = `?apikey=${key}&q=${city}`;
-
-    const response = await fetch(base + query);
-    const data = await response.json();
-
-    return data[0];
-};
-
-
+    }
+}
